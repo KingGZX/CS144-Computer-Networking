4faccd3ec4d85a5c14aae4457a692975c189cbd6
king_gzx <2974127659@qq.com>
2023-05-20 01:24:54 -0700

    lab2 tcp receiver finished, implementations & comments

--- libsponge/tcp_receiver.cc
+++ libsponge/tcp_receiver.cc
@@ -10,10 +10,68 @@ void DUMMY_CODE(Targs &&... /* unused */) {}
 
 using namespace std;
 
+/*
+-----------------IMPORTANT------------------
+
+我们需要的 checkpoint 是定义在 absolute sequence number 空间的 index，包含了 SYN FIN，从0开始且64bit长
+
+而我们的 stream 的 bytes_written() 实际上是 stream index 空间的 index。即实际不把 SYN FIN 写入管道中读取，所以其
+把真实 payload 的第一个 Byte 作为 0 号 index。 整体来说：
+在 FIN 到来之前， index 与 abs seqno 小 1； FIN 之后的数据，小 2.
+
+所以，虽然理论上来讲 我们得到 bytes_written() 后这个值 减去 1 才是最大的已经 reassembled 的 Byte；
+但由于我们忽略了那个 SYN  和 FIN 这个值需要补上 1 或 2 才是 checkpoint （！！因为 checkpoint 是在 abs seqno空间定义的！！）
+*/
+
 void TCPReceiver::segment_received(const TCPSegment &seg) {
-    DUMMY_CODE(seg);
+    // DUMMY_CODE(seg);
+    bool eof = false;
+    const TCPHeader& header = seg.header();
+    WrappingInt32 _headerseq = header.seqno; 
+    // 一般来说 seqno 代表的是payload 荷载数据的第一个Byte的index
+    // 但当头包含 SYN 时，实际上第一个index代表的是 SYN而不是数据！ （SYN 和 FIN 均占用一个index）
+    if(header.syn){
+        _isn = header.seqno;
+        _headerseq = _headerseq + static_cast<uint32_t> (1);
+        _set_isn = true;
+    }
+    if(header.fin){
+        eof = true;
+    }
+    if(_set_isn){   // 只有收到 SYN 后的系列数据包才是有效的
+        // checkpoint 是已经assembled的最大值，可以观察 lab pdf 里的那张图 我们把进入ByteStream的最后一个字节叫做最大的已经reassembled的byte 
+        // 直观来讲 bytes_written() - 1 才是最后一个 index，但由于 stream 中忽略了 SYN ， 我们重新给他加回去
+        uint64_t abs_seq_no = unwrap(_headerseq, _isn, _reassembler.stream_out().bytes_written());
+
+        /*
+        abs_seq_no 是 payload 荷载数据的第一个Byte对应的index
+        要知道 absolute sequence number 第一个index 0 是被 SYN 占用的，
+        但是！
+        StreamBuffer中并不真正装载 SYN，所以实际对应的 abs_seq_no 应该减去 1 才是对应的 stream index，具体可参见 lab2 pdf 表格
+        */
+        
+        _reassembler.push_substring(seg.payload().copy(), abs_seq_no - 1, eof);
+    }
 }
 
-optional<WrappingInt32> TCPReceiver::ackno() const { return {}; }
+optional<WrappingInt32> TCPReceiver::ackno() const { 
+    if(!_set_isn)
+        return {};
+    // 如上所述，单纯的 bytes_written() 忘记考虑了 SYN 占用的字节，所以加上去 
+    // 最后再加上 isn 就是下一个我们希望接收到的 Byte 的 index.
+    uint64_t writtensize = _reassembler.stream_out().bytes_written() + 1;
+    /*
+    这个是因为好像是 收到 FIN 后我们仍希望继续保持通信？？？？
+    总之就是有这样的测试数据
+    那么需要再加上一个 FIN 占用的字节
+    */
+    if(stream_out().input_ended())
+        return WrappingInt32(_isn + static_cast<uint32_t>(writtensize + 1));
 
-size_t TCPReceiver::window_size() const { return {}; }
+    return WrappingInt32(_isn + static_cast<uint32_t>(writtensize)); 
+}
+
+size_t TCPReceiver::window_size() const { 
+    // 就是 已经放入ByteStream中， 但是未被读取的量
+    return stream_out().remaining_capacity(); 
+ }
--- libsponge/tcp_receiver.hh
+++ libsponge/tcp_receiver.hh
@@ -20,12 +20,16 @@ class TCPReceiver {
     //! The maximum number of bytes we'll store.
     size_t _capacity;
 
+    // initial sequence number
+    WrappingInt32 _isn;
+    bool _set_isn;
+
   public:
     //! \brief Construct a TCP receiver
     //!
     //! \param capacity the maximum number of bytes that the receiver will
     //!                 store in its buffers at any give time.
-    TCPReceiver(const size_t capacity) : _reassembler(capacity), _capacity(capacity) {}
+    TCPReceiver(const size_t capacity) : _reassembler(capacity), _capacity(capacity), _isn(0), _set_isn(false) {};
 
     //! \name Accessors to provide feedback to the remote TCPSender
     //!@{
